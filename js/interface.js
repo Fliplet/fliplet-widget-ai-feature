73753c15b80a4c5fc464da811ef0cc11351b3eba
Veljko Popović <veljko_popovic_90@hotmail.com>
2025-10-20 09:54:46 +0200

    ip

--- js/interface.js
+++ js/interface.js
@@ -167,9 +167,9 @@ Fliplet.Widget.generateInterface({
         const debugMode = false;
         debugger
         if (!Fliplet.Helper.field("guid").get()) {
-          Fliplet.Helper.field("guid").set(Fliplet.guid());
-          Fliplet.Widget.save(Fliplet.Widget.fields, { id: widgetId });
-          Fliplet.Studio.emit("reload-page-preview");
+          debugger
+          Fliplet.Studio.emit("reload-widget-instance", widgetId);
+          return;
         }
 
         // Debug utility function to conditionally log console messages
@@ -5035,8 +5035,8 @@ function saveGeneratedCode(parsedContent) {
   data.fields.javascript = parsedContent.javascript;
   data.fields.regenerateCode = true;
   Fliplet.Helper.field("dataSourceId").set("");
-  debugger;
   data.fields.guid = Fliplet.Helper.field("guid").get();
+  debugger;
   return Fliplet.Widget.save(data.fields).then(function () {
     Fliplet.Studio.emit("reload-widget-instance", widgetId);
     // toggleLoaderCodeGeneration(false);
